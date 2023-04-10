b95958dda02a42deedf67c569d8d06f5ade89f0c
Code-X <andrewomosh44@gmail.com>
2023-04-10 13:09:57 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <>
+            Home here...
+        </>
+     );
+}
+ 
+export default Home;
