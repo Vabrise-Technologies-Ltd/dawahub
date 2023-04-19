2ea6b9ad14f0cbb3169c526f11fcfa58c94e04ae
Code-X <andrewomosh44@gmail.com>
2023-04-20 00:25:17 +0300

    add data-file

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
-import About from './pages/About'
 import Account from './pages/Account'
 import Blogs from './pages/Blogs'
 import Cart from './pages/Cart'
 import Checkout from './pages/Checkout'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
+import Prescription from './pages/Prescription'
 import Shop from './pages/Shop'
 
 function App() {
+
 
   return (
     <div className="App">
@@ -21,7 +22,7 @@ function App() {
         <Routes>
           <Route path='/' element={ <Home /> } />
           <Route path='/shop' element={ <Shop /> } />
-          <Route path='/about' element={ <About /> } />
+          <Route path='/prescription' element={ <Prescription /> } />
           <Route path='/cart' element={ <Cart /> } />
           <Route path='/account' element={ <Account /> } />
           <Route path='/checkout' element={ <Checkout /> } />
