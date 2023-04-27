5cf5b5c3db9979cc35c57d62c778ee94187744a8
Code-X <andrewomosh44@gmail.com>
2023-04-27 16:22:34 +0300

    fix: add home heroSection padding

--- src/components/Home/HomeHeroSection.jsx
+++ src/components/Home/HomeHeroSection.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 
 const HomeHeroSection = () => {
     return ( 
-        <div className="md:p-16 p-6 min-h-screen bg-white">
+        <div className="md:px-16 px-6 md:py-2 py-8 min-h-screen bg-white">
             <div className="grid sm:grid-cols-2 lg:gap-16 md:gap-8 gap-4">
                 <div className="flex flex-col justify-center">
-                    <h1 className="lg:text-7xl md:text-5xl text-3xl font-bold">Your Onestop Online Medical Store</h1>
+                    <h1 className="lg:text-7xl md:text-5xl text-3xl font-bold text-gray-800">
+                        Doctors Prescribe, <br />
+                        <span>We Deliver</span>
+                    </h1>
                     <p className="py-6 md:text-lg text-sm">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <Link to='/shop' className="btn md:w-1/4 w-1/2">Shop Now</Link>
                 </div>
